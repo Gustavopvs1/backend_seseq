f8b1514fcc6635b09b6d4a9d00aec5fbbeb65d51
Gustavopvs1 <polen451@gnail.com>
2024-08-04 15:57:22 -0600

    Archivo y endpoint para gestionar salas 2

--- routes/salas.js
+++ routes/salas.js
@@ -3,13 +3,16 @@ const router = express.Router();
 const db = require('../database/db'); // Asegúrate de que esta ruta apunta a tu archivo de conexión de la base de datos
 
 // Obtener todas las salas
-router.get('/salas', async (req, res) => {
-    try {
-        const [rows] = await db.query('SELECT * FROM salas_quirofano');
-        res.json(rows);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
+router.get('/salas', (req, res) => {
+    const query = 'SELECT * FROM salas_quirofano';
+
+    db.query(query, (err, results) => {
+        if (err) {
+            console.error('Error fetching salas:', err);
+            return res.status(500).json({ message: 'Error fetching salas.' });
+        }
+        res.json(results);
+    });
 });
 
 // Actualizar el estado de una sala
